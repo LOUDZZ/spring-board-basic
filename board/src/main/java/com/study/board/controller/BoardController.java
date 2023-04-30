cb027dcc3df054e13a22ea93bd23201e528c0a35
임채연 <dlacodus0407@gmail.com>
2023-04-30 16:22:00 +0900

    board delete

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -41,8 +42,33 @@ public class BoardController {
     //localhost:8080/board/view?id=1
     @GetMapping("/board/view")
     public String boardView(Model model, Integer id) {
-        model.addAttribute("borad", boardService.boardView(id));
+        model.addAttribute("board", boardService.boardView(id));
         return "boardView";
     }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+
+    //{id}가 PathVariable을 통해 들어와 Integer id로 인식 된다.
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardModify";
+    }
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable Integer id, Board board) {
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
+    }
+
 
 }
